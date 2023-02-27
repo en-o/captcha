f8b69a18b970cdcb5e6bb10c866b6a2fcb9f5d76
tan <13320385140@163.com>
2023-02-27 15:24:06 +0800

    feat: 默认接口

--- src/main/java/cn/tannn/CaptchaApplication.java
+++ src/main/java/cn/tannn/CaptchaApplication.java
@@ -1,16 +1,26 @@
 package cn.tannn;
 
+import cn.jdevelops.result.result.ResultVO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author tan
  */
 @SpringBootApplication
+@RestController
 public class CaptchaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CaptchaApplication.class, args);
+    }
+
+
+    @GetMapping
+    public ResultVO<String> hi(){
+        return ResultVO.success("hi");
     }
 
 }
